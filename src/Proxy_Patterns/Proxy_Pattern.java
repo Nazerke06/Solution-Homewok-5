3351de2c2951c561b74be3a934d651a13fd3161a
Nazerke06 <abdumaliknazerke9@gmail.com>
2025-04-04 13:37:37 +0500

    Third commit

--- src/Proxy_Patterns/Proxy_Pattern.java
+++ src/Proxy_Patterns/Proxy_Pattern.java
@@ -1,12 +1,17 @@
 package Proxy_Patterns;
 
 public class Proxy_Pattern implements Image {
+    private FullImage fullImage;
     private String imageName;
 
     public Proxy_Pattern(String imageName) {
         this.imageName = imageName;
     }
     public void display(){
-
+        if (fullImage==null){
+            fullImage= new FullImage(imageName);
+            System.out.println("No image");
+        }
+        fullImage.display();
     }
 }
