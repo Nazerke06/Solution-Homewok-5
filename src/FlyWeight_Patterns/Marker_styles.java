e43d2e44a6f4db5d6c9f666388e521f429bde582
Nazerke06 <abdumaliknazerke9@gmail.com>
2025-04-05 08:26:14 +0500

    Fist of second

---
+++ src/FlyWeight_Patterns/Marker_styles.java
@@ -0,0 +1,14 @@
+package FlyWeight_Patterns;
+
+public class Marker_styles {
+    private String iconType;
+    private String color;
+    private String labelStyle;
+
+    public Marker_styles(String iconType, String color, String labelStyle) {
+        this.iconType = iconType;
+        this.color = color;
+        this.labelStyle = labelStyle;
+    }
+
+}
